beb392daea5220e302ce4309bdba9b63b78a3034
nomadriding <fundora.a@gmail.com>
2024-07-27 15:13:04 -0400

    work on countryDetails

--- src/pages/CountryDetailsPage.jsx
+++ src/pages/CountryDetailsPage.jsx
@@ -1,3 +1,35 @@
-function CountryDetails() {}
+import { useState } from "react";
+import { useParams } from "react-router-dom";
+
+function CountryDetails() {
+    const {countryId} = useParams();
+    const {country, setCountry} = useState();
+    
+    return(
+        <div className="container">
+            <h1 className="page-title">Country Details</h1>
+            <h2>{country.name.common}</h2>
+            <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+             alt={`${country.name.common} flag`} />
+             <p><strong>Capital: </strong> {country.capital[0]}</p>
+             <p><strong>Area: </strong> {country.area} km<sup>2</sup></p>
+             <p>
+                <strong>Borders: </strong>
+                {country.borders.length > 0 ? (
+                    country.borders.map((border, index) => (
+                        <span key={border}>
+                            <Link to={`/${border}`}>{border}</Link>
+                            {index < country.borders.length - 1 && ', '}
+                        </span>
+                    ))
+                ) : (
+                    'None'
+                )}
+             </p>
+             <p><strong>Region: </strong> {country.region}</p>
+             <p><strong>Subregion: </strong> {country.subregion}</p>
+        </div>
+    )
+}
 
 export default CountryDetails;
